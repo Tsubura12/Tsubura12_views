6c34778772d05fcff5d7a405a9511d6dad519580
Tsubura16 <mamt138t@gmail.com>
2025-11-18 10:26:46 +0900

    ブログの投稿フォームを実装

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,21 +1,30 @@
 from flask import Flask
+# SQLAlchemyをインポート
 from flask_sqlalchemy import SQLAlchemy
+# SQLAlchemyのdeclarative_baseをインポート
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-db = SQLAlchemy()
+db = SQLAlchemy()  # ここでインスタンス作成
 
 def create_app():
+    # アプリケーションのインスタンスを作成
     app = Flask(__name__)
+    # 🔑 セッション・flash()・ログイン用に必須
+    app.config['SECRET_KEY'] = 'dev'  # 開発用（本番では安全なランダム文字列に変更）
+
+    # SQLiteのデータベースファイルを指定
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskr.db'
+    # SQLAlchemyの設定を無効化
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)
+    db.init_app(app)  # SQLAlchemyのインスタンスをアプリに紐付け
 
-    # Blueprintの登録
+    # blueprintの登録
     from . import blogs
     app.register_blueprint(blogs.blog_bp)
 
+    # データベースのテーブルを作成
     with app.app_context():
         db.create_all()
 
--- flaskr/blogs.py
+++ flaskr/blogs.py
@@ -1,12 +1,47 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash # <= ここを追加
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flaskr import db
 # models.pyのBlogクラスをインポート
 from flaskr.models import Blog
 
-blog_bp = Blueprint('blogs', __name__, url_prefix='/blogs') # <= ここを変更
+blog_bp = Blueprint('blogs', __name__, url_prefix='/blogs')
 
 # 一覧表示
 @blog_bp.route('/')
 def index():
     blogs = Blog.query.order_by(Blog.created_at.desc()).all()
-    return render_template('blogs/index.html', blogs=blogs) # <= テンプレートパスを変更を追加
+    return render_template('blogs/index.html', blogs=blogs)
+
+# 新規投稿フォーム表示と投稿処理
+@blog_bp.route('/new', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        user_name = request.form['user_name']
+
+        # Blogインスタンスを作成
+        new_blog = Blog(title=title, body=body, user_name=user_name)
+        # バリデーションを実行
+        errors = new_blog.validate()
+
+        # エラーがあればフォームに戻して表示する
+        if errors:
+            # エラーのリストの要素一つずつをflushに入れている
+            for e in errors:
+                flash(e, 'error')
+            return render_template('blogs/new.html', title=title, body=body, user_name=user_name)
+        # DBへ保存
+        db.session.add(new_blog)
+        db.session.commit()
+
+        # 保存後、詳細ページへリダイレクト
+        return redirect(url_for('blogs.detail', blog_id=new_blog.id))
+
+    # blogs/new.htmlをテンプレートとしてHTMLを組み立てる
+    return render_template('blogs/new.html')
+
+# 詳細ページ
+@blog_bp.route('/<int:blog_id>')
+def detail(blog_id):
+    blog = Blog.query.get_or_404(blog_id)
+    return render_template('blogs/detail.html', blog=blog)
